862966a1d1753e6b7d54c8bedb3c2fff77248bcd
aMiscreant <aMiscreant@protonmail.com>
2025-08-31 00:35:38 -0400

    Compile Mainline 6.6 Kernel OrangePi Zero3

--- Kernel/compile_mainline6.6.sh
+++ Kernel/compile_mainline6.6.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
+# aMiscreant
 
 # Settings
 KERNEL_SOURCE="https://github.com/torvalds/linux.git"
 KERNEL_BRANCH="v6.6"
 KERNEL_OUTPUT_DIR="kernel-mainline"
 ORANGE_PI_FIRMWARE="https://github.com/orangepi-xunlong/firmware"
+# Configure custom defconfig
+CUSTOM_DEFCONFIG=""
 
 # Cross compile setup (edit if toolchain differs)
 export ARCH=arm64
@@ -22,7 +25,8 @@ menu () {
   echo "4) Configure Kernel (menuconfig)"
   echo "5) Compile Kernel"
   echo "6) Compile Kernel .debs"
-  echo "7) Exit"
+  echo "7) Clean Kernel Directory"
+  echo "8) Exit"
   echo "=============================="
   read -rp "Select an option: " choice
 
@@ -33,7 +37,8 @@ menu () {
     4) configure_kernel ;;
     5) compile_kernel ;;
     6) compile_kernel_debs ;;
-    7) exit 0 ;;
+    7) compile_clean ;;
+    8) exit 0 ;;
     *) echo "Invalid option"; sleep 2 ;;
   esac
   menu
@@ -87,8 +92,25 @@ configure_kernel() {
     sleep 2
     return
   fi
+
   cd "$KERNEL_OUTPUT_DIR" || exit
-  make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE menuconfig O=out
+
+  if [ -n "$CUSTOM_DEFCONFIG" ]; then
+    CONFIG_PATH="arch/arm64/configs/$CUSTOM_DEFCONFIG"
+    if [ -f "$CONFIG_PATH" ]; then
+      echo "[*] Using custom defconfig: $CONFIG_PATH"
+      make O=out ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE "$CUSTOM_DEFCONFIG"
+    else
+      echo "[!] Custom defconfig $CUSTOM_DEFCONFIG not found in arch/arm64/configs. Using default."
+      make O=out ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+    fi
+  else
+    echo "[*] No custom defconfig set. Using default."
+    make O=out ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE defconfig
+  fi
+
+  echo "[*] Launching menuconfig..."
+  make O=out ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE menuconfig
 }
 
 compile_kernel() {
@@ -113,6 +135,22 @@ compile_kernel_debs() {
   echo "[*] Starting kernel build..."
   make ARCH=$ARCH CROSS_COMPILE=$CROSS_COMPILE bindeb-pkg O=out -j"$(nproc)"
   echo "[+] Kernel build complete"
+}
+
+compile_clean() {
+  if [ ! -d "$KERNEL_OUTPUT_DIR" ]; then
+    echo "[!] Please download kernel source first!"
+    sleep 2
+    return
+  fi
+  if [ ! -d "$KERNEL_OUTPUT_DIR/out" ]; then
+    echo "[*] Cleaning directory..."
+    cd $KERNEL_OUTPUT_DIR && rm -rf out && make ARCH=$ARCH mrproper
+    echo "[+] Directory cleaned."
+  else
+    echo "[=] Directory already cleaned."
+  fi
+  sleep 2
 }
 # ========= Start ========= #
 menu
